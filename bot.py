b343926c72ea431dc687efbc374a32403ba36a9f
HayaseYuuka <dxd@hayaseyuuka.date>
2026-05-03 15:21:31 +0800

    init

---
+++ bot.py
@@ -0,0 +1,18 @@
+import nonebot
+from nonebot.adapters.console import Adapter as CONSOLEAdapter
+from nonebot.adapters.onebot.v11 import Adapter as ONEBOT_V11Adapter
+
+nonebot.init()
+
+driver = nonebot.get_driver()
+driver.register_adapter(ONEBOT_V11Adapter)
+
+driver.register_adapter(CONSOLEAdapter)
+
+nonebot.load_builtin_plugins("echo", "single_session")
+
+
+nonebot.load_from_toml("pyproject.toml")
+
+if __name__ == "__main__":
+    nonebot.run()
