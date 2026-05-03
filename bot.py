5ef6bd9b4e51a0c79b2c089e589373a540dfc5f6
HayaseYuuka <dxd@hayaseyuuka.date>
2026-05-03 18:06:50 +0800

    更新插件

--- bot.py
+++ bot.py
@@ -2,7 +2,6 @@ import nonebot
 from nonebot.adapters.onebot.v11 import Adapter as ONEBOT_V11Adapter
 
 # from nonebot.adapters.console import Adapter as CONSOLEAdapter
-
 nonebot.init()
 
 driver = nonebot.get_driver()
@@ -11,7 +10,6 @@ driver.register_adapter(ONEBOT_V11Adapter)
 # driver.register_adapter(CONSOLEAdapter)
 
 nonebot.load_builtin_plugins("echo", "single_session")
-
 
 nonebot.load_from_toml("pyproject.toml")
 
