6ca381bb6e0ecd1240226d53bbe90e1be1a4b460
HayaseYuuka <dxd@hayaseyuuka.date>
2026-05-03 16:01:03 +0800

    关闭console适配器

--- bot.py
+++ bot.py
@@ -1,13 +1,14 @@
 import nonebot
-from nonebot.adapters.console import Adapter as CONSOLEAdapter
 from nonebot.adapters.onebot.v11 import Adapter as ONEBOT_V11Adapter
+
+# from nonebot.adapters.console import Adapter as CONSOLEAdapter
 
 nonebot.init()
 
 driver = nonebot.get_driver()
 driver.register_adapter(ONEBOT_V11Adapter)
 
-driver.register_adapter(CONSOLEAdapter)
+# driver.register_adapter(CONSOLEAdapter)
 
 nonebot.load_builtin_plugins("echo", "single_session")
 
